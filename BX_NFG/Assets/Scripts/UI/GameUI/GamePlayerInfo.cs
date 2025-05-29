4808bcd99657609932597192c935924e3aa2d722
saulbasc <saulbascor02@gmail.com>
2025-05-29 18:10:42 +0200

    fix: Player Tag set correctly

--- BX_NFG/Assets/Scripts/UI/GameUI/GamePlayerInfo.cs
+++ BX_NFG/Assets/Scripts/UI/GameUI/GamePlayerInfo.cs
@@ -1,19 +1,43 @@
-﻿
-using Assets.Scripts.Init;
+﻿using Assets.Scripts.Init;
 using Assets.Scripts.Lobbi.Logic;
 using TMPro;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
-namespace Assets.Scripts.UI.GameUI
+public class GamePlayerInfo : NetworkBehaviour
 {
-    public class GamePlayerInfo : NetworkBehaviour
+    [SerializeField] private TextMeshProUGUI playerNameTag;
+
+    private NetworkVariable<FixedString32Bytes> playerName = new NetworkVariable<FixedString32Bytes>(
+        "", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    public override void OnNetworkSpawn()
     {
-        [SerializeField] private TextMeshProUGUI playerNameTag;
-        public override void OnNetworkSpawn()
+        playerName.OnValueChanged += OnPlayerNameChanged;
+
+        playerNameTag.text = playerName.Value.ToString();
+
+        if (IsOwner)
         {
             var data = LobbyPlayerManager.Instance.GetSinglePlayerDataObject(UnityServicesActions.GetCurrentUserID());
-            playerNameTag.text = data.GameTag;
+            SetPlayerNameServerRpc(new FixedString32Bytes(data.GameTag));
         }
+    }
+
+    private void OnPlayerNameChanged(FixedString32Bytes oldValue, FixedString32Bytes newValue)
+    {
+        playerNameTag.text = newValue.ToString();
+    }
+
+    [ServerRpc]
+    private void SetPlayerNameServerRpc(FixedString32Bytes name)
+    {
+        playerName.Value = name;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        playerName.OnValueChanged -= OnPlayerNameChanged;
     }
 }
